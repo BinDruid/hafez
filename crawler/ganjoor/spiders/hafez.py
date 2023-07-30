f65c43f8a75b6b9630846e231a16db7a86f66362
BinDruid <abharya.dev@gmail.com>
2023-07-30 19:08:57 +0330

    refactor: change poem item dict name

--- crawler/ganjoor/spiders/hafez.py
+++ crawler/ganjoor/spiders/hafez.py
@@ -9,30 +9,37 @@ class PartialPoem:
     verse_2: str
 
     @property
-    def has_poet_name(self):
+    def includes_poet_name(self):
         return self.POET_NAME in self.verse_1 or self.POET_NAME in self.verse_2
 
 
 class HafezSpider(Spider):
     name = "hafez"
     start_urls = ["https://ganjoor.net/hafez/ghazal/sh1"]
+    poem_path = '//*[@class="b"]//p/text()'
+    poem_name_path = '//*[@id="page-hierarchy"]//h2/a/text()'
+    next_poem_path = '//*[@class="navleft"]/a/@href'
 
     def parse(self, response):
         yield from self.parse_poem(response)
-        next_page = response.xpath('//*[@class="navleft"]/a/@href').get()
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+        next_poem = response.xpath(self.next_poem_path).get()
+        if next_poem is not None:
+            yield response.follow(next_poem, callback=self.parse)
 
     def parse_poem(self, response):
-        poem = response.xpath('//*[@class="b"]//p/text()').extract()
-        poem_name = response.xpath('//*[@id="page-hierarchy"]//h2/a/text()').get().strip()
+        poem = response.xpath(self.poem_path).extract()
+        poem_name = response.xpath(self.poem_name_path).get().strip()
         start_couplet = poem[0]
         autobiographical_reference = []
         for verse_no in range(0, len(poem) - 1, 2):
             verse_1 = poem[verse_no]
             verse_2 = poem[verse_no + 1]
             poem_part = PartialPoem(verse_1, verse_2)
-            if poem_part.has_poet_name:
+            if poem_part.includes_poet_name:
                 autobiographical_reference.extend([verse_1, verse_2])
         if autobiographical_reference:
-            yield {"شماره": poem_name, "مطلع": start_couplet, "تلخیص": autobiographical_reference}
+            yield {
+                "poem_name": poem_name,
+                "start_couplet": start_couplet,
+                "autobiographical_reference": autobiographical_reference,
+            }
